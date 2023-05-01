59eaff02f4e13539760fba5e39d0bb5048e02bd6
Rimtex <Raider@ukr.net>
2023-05-02 01:07:22 +0300

    переменные адресов для файлов
    Signed-off-by: Rimtex <Raider@ukr.net>

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 path_to_shortcut = "ярлыки\\"            # стандартная папка ярлыков
-ideas = "F:\\Rimtex\\блокнот\\идеи.txt"  # путь для работы с текстовым файлом
+ideas = "идеи.txt"                       # путь для работы с текстовым файлом
 dir_path = r"E:\Muzic"                   # для проигрывания музыки!
 requirements_path = "F:\\Rimtex\\Projects\\Voice_Commands\\requirements.txt"  # для команд библиотек
 
