c76f4b1045facad14cb0e645bf567c01b69b5435
Rimtex <Raider@ukr.net>
2023-05-03 15:41:42 +0300

    _#   2#   2
    Signed-off-by: Rimtex <Raider@ukr.net>

--- Voice_Commands.py
+++ Voice_Commands.py
@@ -1024,7 +1024,7 @@ if __name__ == '__main__':
                     win32clipboard.OpenClipboard()
                     Commit = win32clipboard.GetClipboardData(win32clipboard.CF_UNICODETEXT)
                     win32clipboard.CloseClipboard()
-                    key_write(Commit)
+                    pyautogui.typewrite(Commit)
                     click_print_coordinates(1478, 1325)  # кнопка Commit and Push...
                     time.sleep(2)
                     click_print_coordinates(1478, 1325)  # кнопка Push там же
