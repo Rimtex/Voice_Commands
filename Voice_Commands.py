3c3db4eae7a9dafd8a519c8f754c67aa75a0a448
Rimtex <Raider@ukr.net>
2023-05-04 17:05:00 +0300

    _
    Signed-off-by: Rimtex <Raider@ukr.net>

--- Voice_Commands.py
+++ Voice_Commands.py
@@ -309,7 +309,7 @@ if __name__ == '__main__':
                             key_press("enter")
                         except Exception as e:
                             print(traceback.format_exc())
-                            print(f" (!o_O): {LRE}! переводчик не работает с Вэ!Пэ!эН! ", e)
+                            print(f" (!o_O): {LRE}! переводчик не работает с Planet VPN ", e)
                     #: окей гугл
                     elif len(words) >= 2 and (words[0] == 'окей' and words[1] == 'гугл'):
                         if prompt == '"окей гугл"':
