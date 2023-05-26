97def73fe10b22f594be002b54d2a44b7536b59a
Rimtex <Raider@ukr.net>
2023-05-26 16:12:30 +0300

    _
    Signed-off-by: Rimtex <Raider@ukr.net>

--- address_config.py
+++ address_config.py
@@ -5,7 +5,7 @@ dir_path = r"E:\Muzic"                                  # ! путь для пр
 assistant_window = "ассистент"                          # название ярлыка для работы окна ассистента
 requirements_path = "F:\\Rimtex\\Projects\\Voice_Commands\\requirements.txt"  # ! для команд библиотек
 
-model1 = r"vosk-model-small-ru-0.22"
-model2 = r"vosk-model-small-en-us-0.15"
-model3 = r"vosk-model-ru-0.42"
-model4 = r"vosk-model-en-us-0.22"
+model1 = r"voskmodels\vosk-model-small-ru-0.22"
+model2 = r"voskmodels\vosk-model-small-en-us-0.15"
+model3 = r"voskmodels\vosk-model-ru-0.42"
+model4 = r"voskmodels\vosk-model-en-us-0.22"
