1ca57d28817cf60c0be56c9fc2898a8952212d31
Rimtex <Raider@ukr.net>
2024-01-08 19:10:34 +0300

    _

--- address_config.py
+++ address_config.py
@@ -4,7 +4,7 @@ reminder = path_to_shortcut + "напоминалка.txt"  # 2
 assistant_window = "ассистент"                   # название ярлыка для работы окна ассистента
 models_directory = "voskmodels"
 dir_path = r"E:\Muzic"                                                        # ! путь для проигрывания музыки
-requirements_path = "F:\\Rimtex\\Projects\\Voice_Commands\\requirements.txt"  # ! для команд библиотек
+requirements_path = "F:\\Rimtex\\Projects\\Voice_Commands.py\\requirements.txt"  # ! для команд библиотек
 
 model1 = r"voskmodels\vosk-model-small-ru-0.22"
 model2 = r"voskmodels\vosk-model-small-en-us-0.15"
