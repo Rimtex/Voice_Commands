7befab3bf19ad69c38a3f0cdb3178866bb2f92c6
Rimtex <Raider@ukr.net>
2024-01-08 20:25:30 +0300

    _

--- Сonfig.py
+++ Сonfig.py
@@ -1,11 +1,10 @@
-path_to_shortcut = "ярлыки\\"                    # папка ярлыков
-ideas = path_to_shortcut + "идеи.txt"            # путь для работы команд с текстовым файлом 1
+path_to_shortcut = "ярлыки\\"  # папка ярлыков
+ideas = path_to_shortcut + "идеи.txt"  # путь для работы команд с текстовым файлом 1
 reminder = path_to_shortcut + "напоминалка.txt"  # 2
-assistant_window = "ассистент"                   # название ярлыка для работы окна ассистента
+assistant_window = "ассистент"  # название ярлыка для работы окна ассистента
 models_directory = "voskmodels"
-dir_path = r"E:\Muzic"                                                        # ! путь для проигрывания музыки
+dir_path = r"E:\Muzic"  # ! путь для проигрывания музыки
 requirements_path = "F:\\Rimtex\\Projects\\Voice_Commands.py\\requirements.txt"  # ! для команд библиотек
-
 model1 = r"voskmodels\vosk-model-small-ru-0.22"
 model2 = r"voskmodels\vosk-model-small-en-us-0.15"
 model3 = r"voskmodels\vosk-model-ru-0.42"
