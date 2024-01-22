267a0fc204e9577b4d8e88ce1cf8e1385977bee1
Rimtex <Raider@ukr.net>
2024-01-22 16:27:51 +0300

    _

--- setup_config.py
+++ setup_config.py
@@ -37,15 +37,15 @@ def create_shortcut(shortcut_name, target_script_path):
         shortcut_if_create.moveTo(88, 220)
         shortcut_if_create.resizeTo(849, 327)
     except Exception as e:
-        print(
-            f"\r                                                   (!o_O) ярлык --> {shortcut_name}\r")
+        print(f"\r                                               (!o_O) ярлык --> {shortcut_name}\r")
         script_path = target_script_path
         script_directory = os.path.dirname(script_path)
         shortcut_name_link_path = os.path.join(script_directory, path_to_shortcut + shortcut_name + ".lnk")
         if not os.path.isfile(shortcut_name_link_path):
             shell = win32com.client.Dispatch("WScript.Shell")
             shortcut = shell.CreateShortCut(shortcut_name_link_path)
-            shortcut.TargetPath = script_path
+            shortcut.TargetPath = "python.exe"
+            shortcut.Arguments = script_path
             shortcut.Description = shortcut_name
             shortcut.WorkingDirectory = script_directory
             shortcut.Save()
